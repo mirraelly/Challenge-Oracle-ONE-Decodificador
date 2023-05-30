78aaa0ab13f850512a8dfd38f93b7061ff7c945c
Mirraely <mirraelymoura@gmail.com>
2023-05-30 02:49:45 -0300

    Atualizando projeto

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function criptografar(texto) {
     let resultado = texto.replaceAll("e", "enter");
     resultado = resultado.replaceAll("i", "imes");
     resultado = resultado.replaceAll("a", "ai");
-    resultado = resultado.replaceAll("o", "ober"); 
+    resultado = resultado.replaceAll("o", "ober");
     resultado = resultado.replaceAll("u", "ufat");
     return resultado;
 }
@@ -16,10 +16,19 @@ function descriptografar(texto) {
     return resultado;
 }
 
-function validaEntrada(texto){
+function validaEntrada(texto) {
+    if (texto === '') {
+        return false;
+    }
+
+    const acentos = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëÇçðÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
+
+    if (texto.split('').some(caractere => acentos.indexOf(caractere) >= 0)) {
+        return false;
+    }
+
     let minusculo = texto.toLowerCase();
-    let maiusculo = texto.toUpperCase();
-    if(texto == minusculo){
+    if (texto == minusculo) {
         return true;
     } else {
         return false;
@@ -32,3 +41,63 @@ function testar() {
 }
 
 // console.log(criptografar("paralelepipedo quebrado")); exemplo: "paralelepipedo quebrado"
+
+
+const textareaCriptografar = document.querySelector('#textoCriptografar');
+const textareaCriptografado = document.querySelector('#textoCriptografado');
+const divNenhumTexto = document.querySelector('.nenhum-texto');
+const divTextoCriptografia = document.querySelector('.texto-criptografia');
+
+const buttonCriptografar = document.querySelector('#criptografar');
+const buttonDescriptografar = document.querySelector('#descriptografar');
+const buttonApagar = document.querySelector('#apagar');
+const buttonCopiar = document.querySelector('#copiar');
+
+buttonCriptografar.addEventListener('click', () => {
+    const textoOriginal = textareaCriptografar.value;
+
+    if (validaEntrada(textoOriginal)) {
+        const textoFinal = criptografar(textoOriginal);
+
+        textareaCriptografado.value = textoFinal;
+        divNenhumTexto.classList.remove('display-block');
+        divNenhumTexto.classList.add('display-none');
+        divTextoCriptografia.classList.remove('display-none');
+        divTextoCriptografia.classList.add('display-block');
+    } else {
+        alert('O texto digitado possui letras maiúsculas e/ou com acentos.\n' +
+            'Por favor, digite um texto válido!')
+    }
+});
+
+buttonDescriptografar.addEventListener('click', () => {
+    const textoOriginal = textareaCriptografar.value;
+
+    if (validaEntrada(textoOriginal)) {
+        const textoFinal = descriptografar(textoOriginal);
+
+        textareaCriptografado.value = textoFinal;
+        divNenhumTexto.classList.remove('display-block');
+        divNenhumTexto.classList.add('display-none');
+        divTextoCriptografia.classList.remove('display-none');
+        divTextoCriptografia.classList.add('display-block');
+    } else {
+        alert('O texto digitado possui letras maiúsculas e/ou com acentos.\n' +
+            'Por favor, digite um texto válido!')
+    }
+});
+
+buttonCopiar.addEventListener('click', async () => {
+    const texto = textareaCriptografado.value;
+
+    await navigator.clipboard.writeText(texto);
+});
+
+buttonApagar.addEventListener('click', () => {
+    textareaCriptografar.value = '';
+    textareaCriptografado.value = '';
+    divNenhumTexto.classList.remove('display-none');
+    divNenhumTexto.classList.add('display-block');
+    divTextoCriptografia.classList.remove('display-block');
+    divTextoCriptografia.classList.add('display-none');
+});
