c24213f121273f35de29b03821108fea9e3b8ced
Mirraely <mirraelymoura@gmail.com>
2023-05-30 12:25:25 -0300

    Atualizações da documentação do projeto

--- README.md
+++ README.md
@@ -1,5 +1,43 @@
-# Challenge ONE Sprint 01
+# Challenge ONE - Decodificador de Texto
 
-## Contruindo um decodificador de texto em JavaScript
+O desafio da Sprint 01 do programa ONE (uma parceria entre a Oracle e a Alura) foi a construção de um codificador de texto em JavaScript sem o uso de frameworks.
 
-Este é o repositório do meu decodificador. Para conferí-lo, [clique aqui](https://mirraelly.github.io/Challenge-Oracle-ONE/).
+Para conferir como ficou o meu projeto, [clique aqui](https://mirraelly.github.io/Challenge-Oracle-ONE/).
+
+## Tecnologias utilizadas
+<div style="display: inline_block"><br>
+  <img align="center" alt="Mirra-Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+  <img align="center" alt="Mirra-HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
+  <img align="center" alt="Mirra-CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg">
+</div><br>
+
+## Requisitos:
+
+* Funcionais: 
+
+    * Deve funcionar apenas com letras minúsculas
+    * Não devem ser utilizados letras com acentos nem caracteres especiais
+    * Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original.
+
+* Não Funcionais: 
+
+    * A página deve ter campos para inserção do texto a ser criptografado ou descriptografado, e a pessoa usuária deve poder escolher entre as duas opções
+    * O resultado deve ser exibido na tela.
+
+* Funcionalidade extra: 
+
+    * Um botão que copie o texto criptografado/descriptografado para a área de transferência - ou seja, que tenha a mesma funcionalidade do ctrl+C ou da opção "copiar" do menu dos aplicativos.
+
+## Ferramentas utilizadas
+
+* [Trelo](https://trello.com/b/EmUFmjCv/decodificador-de-texto-alura-challenges-oracle-one)
+* Discord exclusivo de Alunos ONE
+* [Modelo Figma](https://www.figma.com/file/tvFEYhVfZTjdJ5P24RGV21/Alura-Challenge---Desafio-1---L%C3%B3gica?type=design&node-id=16-802&t=FmKqIa7WE3R4CUgW-0)
+
+## Agradecimentos: 
+
+* [Programa ONE](https://www.oracle.com/br/education/oracle-next-education/)
+* [Oracle](https://www.oracle.com/)
+* [Alura](https://www.alura.com.br/)
+* [Génesys Rondón](https://github.com/genesysaluralatam)
+* [Weslley Bastos](https://github.com/wessbastos)
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+
+/**
+ * Função de criptografia. 
+ * A implementação atual realiza operações de substituição 
+ * de caracteres para ofuscar o texto original.
+ * Nota: A ordem da substituição das vogais interfere no resultado correto.
+ * @param {string} texto 
+ * @returns retorna a string criptografada
+ */
 function criptografar(texto) {
     let resultado = texto.replaceAll("e", "enter");
     resultado = resultado.replaceAll("i", "imes");
@@ -7,6 +16,14 @@ function criptografar(texto) {
     return resultado;
 }
 
+/**
+ * Função de descriptografar. 
+ * A implementação atual realiza operações de substituição 
+ * de caracteres para retornar o texto ofuscado a sua forma original.
+ * Nota: A ordem da substituição das vogais interfere no resultado correto.
+ * @param {string} texto 
+ * @returns retorna a string descriptografada
+ */
 function descriptografar(texto) {
     let resultado = texto.replaceAll("enter", "e");
     resultado = resultado.replaceAll("imes", "i");
@@ -16,6 +33,13 @@ function descriptografar(texto) {
     return resultado;
 }
 
+/**
+ * Função de validação. 
+ * A implementação atual realiza a verificação de caracteres proibidos.
+ * @param {string} texto 
+ * @returns retorna verdadeiro, caso o texto de entrada não contenha caracteres proibidos.
+ * E falso, caso contrário.
+ */
 function validaEntrada(texto) {
     if (texto === '') {
         return false;
@@ -35,64 +59,58 @@ function validaEntrada(texto) {
     }
 }
 
-function testar() {
-    console.assert(criptografar("arroz") == "airroberz", "A criptografia está quebrada");
-    console.assert(descriptografar("enter") == "e", "A descriptografia está quebrada");
-}
-
-// console.log(criptografar("paralelepipedo quebrado")); exemplo: "paralelepipedo quebrado"
-
-
+// Variáveis para armazenar elementos da página.
 const textareaCriptografar = document.querySelector('#textoCriptografar');
 const textareaCriptografado = document.querySelector('#textoCriptografado');
 const divNenhumTexto = document.querySelector('.nenhum-texto');
 const divTextoCriptografia = document.querySelector('.texto-criptografia');
 
+// Variáveis para armazenar elementos que o usuário utiliza.
 const buttonCriptografar = document.querySelector('#criptografar');
 const buttonDescriptografar = document.querySelector('#descriptografar');
 const buttonApagar = document.querySelector('#apagar');
 const buttonCopiar = document.querySelector('#copiar');
 
+// Registrando o evento de clique para quando o usuário 
+// solicita a criptografia do texto digitado. 
 buttonCriptografar.addEventListener('click', () => {
     const textoOriginal = textareaCriptografar.value;
 
     if (validaEntrada(textoOriginal)) {
         const textoFinal = criptografar(textoOriginal);
 
-        textareaCriptografado.value = textoFinal;
-        divNenhumTexto.classList.remove('display-block');
-        divNenhumTexto.classList.add('display-none');
-        divTextoCriptografia.classList.remove('display-none');
-        divTextoCriptografia.classList.add('display-block');
+        atualizarInterface(textoFinal);
     } else {
         alert('O texto digitado possui letras maiúsculas e/ou com acentos.\n' +
             'Por favor, digite um texto válido!')
     }
 });
 
+// Registrando o evento de clique para quando o usuário 
+// solicita a descriptografia do texto digitado. 
 buttonDescriptografar.addEventListener('click', () => {
     const textoOriginal = textareaCriptografar.value;
 
     if (validaEntrada(textoOriginal)) {
         const textoFinal = descriptografar(textoOriginal);
-
-        textareaCriptografado.value = textoFinal;
-        divNenhumTexto.classList.remove('display-block');
-        divNenhumTexto.classList.add('display-none');
-        divTextoCriptografia.classList.remove('display-none');
-        divTextoCriptografia.classList.add('display-block');
+        
+        atualizarInterface(textoFinal);
     } else {
         alert('O texto digitado possui letras maiúsculas e/ou com acentos.\n' +
             'Por favor, digite um texto válido!')
     }
 });
 
+// Registra o evento de clique para quando o usuário
+// solicita a cópia do texto criptografado. 
 buttonCopiar.addEventListener('click', async () => {
     const texto = textareaCriptografado.value;
 
     await navigator.clipboard.writeText(texto);
 });
 
+// Registra o evento de clique para quando o usuário
+// solicita a limpeza dos campos da interface. 
 buttonApagar.addEventListener('click', () => {
     textareaCriptografar.value = '';
     textareaCriptografado.value = '';
@@ -101,3 +119,15 @@ buttonApagar.addEventListener('click', () => {
     divTextoCriptografia.classList.remove('display-block');
     divTextoCriptografia.classList.add('display-none');
 });
+
+/**
+ * Atualiza a interface do usuário com o resultado das operações.
+ * @param {string} textoFinal texto para ser apresentado no painel direito
+ */
+function atualizarInterface(textoFinal) {
+    textareaCriptografado.value = textoFinal;
+    divNenhumTexto.classList.remove('display-block');
+    divNenhumTexto.classList.add('display-none');
+    divTextoCriptografia.classList.remove('display-none');
+    divTextoCriptografia.classList.add('display-block');
+}
